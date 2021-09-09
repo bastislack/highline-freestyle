e91c7987cb4e45f68025947c41aaeb984b79284b
Fölix <60603136+Felix-Hmpl@users.noreply.github.com>
2021-09-10 00:36:34 +0200

    Fix navbar to bottom of screen

--- src/TrickList.js
+++ src/TrickList.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 const TrickList = ({ tricks }) => {
 
   return (
-    <div className="trick-list">
+    <div className="justify-content-evenly row">
       {tricks.map(trick => (
-        <button className="trick-preview" key={trick.id} >
-          <Link className="link-to-trick" to={`/tricks/${trick.id}`}>
-            <h2>{ trick.name }</h2>
-          </Link>
-        </button>
+          <button class="col-md-3 btn btn-outline-success trick-preview" key={trick.id} >
+            <Link className="link-to-trick" to={`/tricks/${trick.id}`}>
+              <h2>{ trick.name }</h2>
+            </Link>
+          </button>
       ))}
     </div>
   );
