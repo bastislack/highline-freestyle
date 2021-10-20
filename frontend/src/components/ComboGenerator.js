8dd37678a3e47e4649bff693d51f4163a86bc801
Axel Weber <weberal@studi.informatik.uni-stuttgart.de>
2021-10-20 17:38:42 +0200

    comboGenerator refactoring
    closes #26, closes #32
    
    Now the generator retries a certain amount of times (now 100) until it
    finds a fitting combo.
    It also automatically checks if the combo is valid. (mentioned in #31)
    
    Also added the two options of, not removing tricks and allowing
    consecutive tricks. These need to be implemented into the interface
    still.

--- frontend/src/components/ComboGenerator.js
+++ frontend/src/components/ComboGenerator.js
@@ -27,42 +27,86 @@ const ComboGenerator = () => {
   const generateCombo = (e) => {
     e.preventDefault();
 
-    if (numberOfTricks < 2) {
-      console.log("You need more than one trick for a combo!");
+    if (numberOfTricks < 1) {
+      alert("the number of tricks can't be negative or 0");
+      return;
+    }else if (numberOfTricks == 1) {
+      alert("You need more than one trick for a combo!");
       return;
     }
 
+    //TODO: needs to be set by the user
+    // settings
+    let removeTricks = false;
+    let allowConsecutiveTricks = false;
+
     let randomCombo = new Array(numberOfTricks);
+    let myTricks = [...tricks];
+
+    let stuckPos;
+    let removedTrick;
+    
+    // maximum retries until the generator stops
+    let maxRetries = 100;
+    let retries = 0;
 
     // Shuffle array of tricks
-    tricks.sort(function(a, b){return 0.5 - Math.random()});
+    let shuffleTricks = () => myTricks.sort((a,b) => 0.5-Math.random());
+    shuffleTricks();
 
     // Get the first trick for the random combo
-    randomCombo[0] = tricks.shift();
+    randomCombo[0] = myTricks[0];
+    if (removeTricks) removedTrick = myTricks.shift();
     
     // Iteratively shuffle array of tricks and find the first trick
     // that has a starting position that matches with the
     // ending position of the trick before
-    // 
-    // Right now the tricks that are used in the random combo
-    // are removed from the tricks array so every trick is only
-    // used once in the random combo
+    //
+    // If it cant find a continuation it will try again until maxRetries
     for (let i = 1; i < numberOfTricks; i++) {
+      if (retries > maxRetries) return alert("coun't find combo");
+
       let trickFound = false;
-      tricks.sort(function(a, b){return 0.5 - Math.random()});
-      for (let j = 0; j < tricks.length; j++) {
-        if (tricks[j].startPos === randomCombo[i-1].endPos) {
-          randomCombo[i] = tricks[j];
-          tricks.splice(j,1);
+      shuffleTricks();
+      let lastPos = randomCombo[i-1].endPos;
+      for (let j = 0; j < myTricks.length; j++) {
+        if (myTricks[j].startPos === lastPos && (allowConsecutiveTricks || randomCombo[i-1] != myTricks[j])) {
+          randomCombo[i] = myTricks[j];
+          if (removeTricks) removedTrick = myTricks.splice(j,1);
           trickFound = true;
+          console.log("found trick");
           break;
         }
       }
+      // if no trick is found the last iteration of the loop is repeated
+      // but if it gets stuck on the same position twice, it starts from the begingin
       if (!trickFound) {
-        console.log("No suitable trick found");
-        return;
+        retries++;
+        console.log("No suitable trick, from position: " + lastPos + " found, removing last trick");
+        if (i == 1 || lastPos == stuckPos) {
+          console.log("starting new");
+          myTricks = [...tricks];
+          shuffleTricks();
+          randomCombo[0] = myTricks[0];
+          i = 0;
+        } else {
+          if (removeTricks) myTricks.push(removedTrick);
+          i = i - 2;
+        }
+        stuckPos = lastPos;
       }
     }
+
+    //check integrety of combo
+    for (let i in randomCombo) {
+      if (i == 0) continue;
+      let prev = randomCombo[i-1];
+      let trick = randomCombo[i];
+      if (prev.endPos != trick.startPos) {
+        alert("trick generator is broken");
+      }
+    }
+
     history.push({
       pathname: '/random-combo',
       state: { combo: randomCombo}
