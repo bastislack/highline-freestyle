abf355ae77b3d1afe49bd075ff17996970292aa8
Fölix <60603136+Felix-Hmpl@users.noreply.github.com>
2021-10-27 17:46:31 +0200

    Let BottomNav indicate current page

--- frontend/src/components/BottomNav.js
+++ frontend/src/components/BottomNav.js
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const BottomNav = () => {
+
+  const path = useLocation().pathname.toString().toLowerCase();
+
+  let currentPage = path; 
+
   return (
     <nav className="bottomNav fixed-bottom justify-content-evenly">
         <Link to={`/`} className="col">
-          <h1>Tricks</h1>
+          <h1 className={(path === "/" | path.includes("tricks") | path === "/createtrick") && "nav-elem-selected"}>Tricks</h1>
         </Link>
         <Link to={`/combos`} className="col">
-          <h1>Combos</h1>
+          <h1 className={(path.includes("/combos") | path === "/createcombo") && "nav-elem-selected"}>Combos</h1>
         </Link>
         <Link to={`/generator`} className="col">
-          <h1>Combo Generator</h1>
+          <h1 className={(path === "/generator" | path === "/random-combo") && "nav-elem-selected"}>Combo Generator</h1>
         </Link>
     </nav>
   );
