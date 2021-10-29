8bae9feb06caeb76df0aba17aa6eda6c11f2d0fe
Felix-Hmpl <60603136+Felix-Hmpl@users.noreply.github.com>
2021-10-30 00:41:42 +0200

    Show FloatingActionButton only at tricklist and combolist

--- src/components/FloatingActionButton.js
+++ src/components/FloatingActionButton.js
@@ -4,23 +4,34 @@ const FloatingActionButton = () => {
   // the current Url
   const path = useLocation().pathname.toString().toLowerCase();
 
-  // definie hiere to where the button leads, depending on the current path
-  let create = path; 
-  if (path === "/") {
-    create = "/createtrick";
-  }else if (path === "/combos") {
-    create = "/createcombo";
-  } else {
-    console.log("no path for the FloatingActionButton has been defined");
+  // show button only at specific paths
+  var buttonVisible = false
+  if (path === "/" || path === "/combos") {
+    buttonVisible = true
   }
 
-  return (
-    <Link to={create}>
-      <button className="kc_fab_main_btn">  
-        {"+"}
-      </button>
-    </Link>
-  )
+  if (buttonVisible === true) {
+    // definie hiere to where the button leads, depending on the current path
+    let create = path;
+    if (path === "/") {
+      create = "/createtrick";
+    } else if (path === "/combos") {
+      create = "/createcombo";
+    } else {
+      console.log("no path for the FloatingActionButton has been defined");
+    }
+
+    return (
+      <Link to={create}>
+        <button className="kc_fab_main_btn">
+          {"+"}
+        </button>
+      </Link>
+    )
+  }
+
+  return (null)
+
 }
 
 export default FloatingActionButton;
