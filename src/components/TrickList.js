29e0e707c94413d295a8711240ec991d432e3047
Axel Weber <weberal@studi.informatik.uni-stuttgart.de>
2021-11-05 17:30:03 +0100

    added multiple sorting options
    this fixes #44

--- src/components/TrickList.js
+++ src/components/TrickList.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 import { useLiveQuery } from "dexie-react-hooks";
 
 import Database from "../services/db";
@@ -6,30 +7,56 @@ const db = new Database();
 
 const TrickList = () => {
 
+  const [sortOpt, setSortOpt] = useState("levelUp");
+
+  // the schemes with the sorting function, the sorting name and a function to the attribute
+  const schemes = {
+    "levelUp": {
+      "sortFunc": (a, b) => (a.difficultyLevel - b.difficultyLevel),
+      "name": "Level",
+      "attributeFunc": (a) => a.difficultyLevel,
+      "showCategory": true,
+    },
+    "levelDown": {
+      "sortFunc": (a, b) => (b.difficultyLevel - a.difficultyLevel),
+      "name": "Level",
+      "attributeFunc": (a) => a.difficultyLevel,
+      "showCategory": true,
+    },
+    "stickUp": {
+      "sortFunc": (a, b) => {if (a.stickFrequency) return (a.stickFrequency - b.stickFrequency);return 0.1;},
+      "name": "StickFrequency",
+      "attributeFunc": (a) => a.stickFrequency,
+      "showCategory": false,
+    },
+    "stickDown": {
+      "sortFunc": (a, b) => {if (a.stickFrequency) return (b.stickFrequency - a.stickFrequency);return 1;},
+      "name": "StickFrequency",
+      "attributeFunc": (a) => a.stickFrequency,
+      "showCategory": false,
+    },
+  };
+
   // tricks query with react hooks -- means it refreshes automaticly
-  const tricks = useLiveQuery(() => db.getAllTricks(), []);
+  // and sorts it according to the sortOpt
+  const tricks = useLiveQuery(() => db.getAllTricks().then(t => t.sort(schemes[sortOpt].sortFunc)), [sortOpt]);
   if (!tricks) {return null} else console.log(tricks);
 
-  let previousDifficultyLevel = 0;
+
+  let current;
 
   return (
     <div className="justify-content-evenly">
 
       <div className="row">
         {tricks.map(trick => {
-          let isFirstOfLevel = false;
 
-          if (trick.difficultyLevel === previousDifficultyLevel) {
-            isFirstOfLevel = false;
-          } else {
-            isFirstOfLevel = true;
-          }
-
-          previousDifficultyLevel = trick.difficultyLevel
+          let isFirst = (schemes[sortOpt].attributeFunc(trick) !== current);
+          current = schemes[sortOpt].attributeFunc(trick);
 
           return (
             <div key={trick.id}>
-              {isFirstOfLevel && <div>Level {trick.difficultyLevel}</div>}
+              {isFirst && schemes[sortOpt].showCategory && <div>{schemes[sortOpt].name} {current}</div>}
               <div>
                 <Link className="link-to-trick " to={`/tricks/${trick.id}`} key={trick.id} >
                   <button className=" btn btn-outline-success trick-preview skillFreq" freq={trick.stickFrequency}>
