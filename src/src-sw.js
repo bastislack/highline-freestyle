a9f8e3228930c563b8954b5b41e575b95fb139fd
Sebastian Egger <ge46pik@tum.de>
2021-11-06 08:48:05 +0100

    Add workbox-recipes

--- src/src-sw.js
+++ src/src-sw.js
@@ -1,5 +1,10 @@
 import {clientsClaim} from 'workbox-core';
 import {precacheAndRoute} from 'workbox-precaching';
+import {
+  pageCache,
+  imageCache,
+  staticResourceCache,
+} from 'workbox-recipes';
 
 // This clientsClaim() should be at the top level
 // of your service worker, not inside of, e.g.,
@@ -9,3 +14,9 @@ clientsClaim();
 self.skipWaiting();
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+pageCache();
+
+staticResourceCache();
+
+imageCache();
