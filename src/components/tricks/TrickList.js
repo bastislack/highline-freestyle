e4be67ef432d0d7b65b94b1a67a0723972793db5
Fölix <60603136+Felix-Hmpl@users.noreply.github.com>
2021-11-14 16:44:31 +0100

    Style TrickList
    Two rows on big screens, one on small screens

--- src/components/tricks/TrickList.js
+++ src/components/tricks/TrickList.js
@@ -9,8 +9,19 @@ const TrickList = ({ sortingSchemes, sortOpt }) => {
   // tricks query with react hooks -- means it refreshes automaticly
   // and sorts it according to the sortOpt
   const tricks = useLiveQuery(() => db.getAllTricks().then(t => t.sort(sortingSchemes[sortOpt].sortFunc)), [sortOpt]);
-  if (!tricks) {return null} else console.log(tricks);
+  if (!tricks) { return null } else console.log(tricks);
 
+  function getTrickDiv(trick) {
+    return (<div key={trick.id} className="col-md-6 col-sm-12">
+      <div>
+        <Link className="link-to-trick " to={`/tricks/${trick.id}`} key={"trick" + trick.id} >
+          <button className=" btn btn-outline-success trick-preview skillFreq" freq={trick.stickFrequency}>
+            <h2>{trick.alias || trick.technicalName}</h2>
+          </button>
+        </Link>
+      </div>
+    </div>)
+  }
 
   let current;
 
@@ -23,22 +34,21 @@ const TrickList = ({ sortingSchemes, sortOpt }) => {
           let isFirst = (sortingSchemes[sortOpt].attributeFunc(trick) !== current);
           current = sortingSchemes[sortOpt].attributeFunc(trick);
 
-          return (
-            <div key={trick.id}>
-              {isFirst && sortingSchemes[sortOpt].showCategory && <h5>{sortingSchemes[sortOpt].catName} {current}</h5>}
-              <div>
-                <Link className="link-to-trick " to={`/tricks/${trick.id}`} key={"trick" + trick.id} >
-                  <button className=" btn btn-outline-success trick-preview skillFreq" freq={trick.stickFrequency}>
-                    <h2>{trick.alias || trick.technicalName}</h2>
-                  </button>
-                </Link>
-              </div>
-            </div>
-          );
+          if (isFirst && sortingSchemes[sortOpt].showCategory) {
+            return [
+              <div className="col-md-6 col-sm-12 w-100"><h5>{sortingSchemes[sortOpt].catName} {current}</h5></div>,
+              getTrickDiv(trick)
+            ];
+          } else {
+            return (
+              getTrickDiv(trick)
+            );
+          }
+
         })}
       </div>
     </div>
   );
 }
-
+//{isFirst && sortingSchemes[sortOpt].showCategory && <h5>{sortingSchemes[sortOpt].catName} {current}</h5>}
 export default TrickList;
