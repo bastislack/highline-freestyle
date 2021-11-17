126481dc1cce356add7035207a009d241203f28b
Rory Stephenson <rorystephenson@gmail.com>
2021-11-18 00:16:17 +0100

    Fix router config

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   const [sortOpt, setSortOpt] = useState(0);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/highline-freestyle">
       <div className="App">
         <Navbar>
           <div style={{display: "flex", alignItems: "center", width: "100%"}}>
