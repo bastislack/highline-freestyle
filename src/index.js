8bd9169f5dea91734238f85e8e6ed11eca79d57c
Rory Stephenson <rorystephenson@gmail.com>
2021-11-18 22:20:12 +0100

    Fix bootstrap config
    1. Import it first to make sure it gets applied to all components.
    2. Don't import it in public/index.html since it is already compiled in
       by webpack when imported in index.js.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './serviceWorkerRegistration';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(
