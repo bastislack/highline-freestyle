a3a07722aa492c8ae7ad4dafb1765d22abfbeae3
Sebastian Egger <ge46pik@tum.de>
2021-12-06 20:03:35 +0100

    Update about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import Modal from 'react-bootstrap/Modal';
+import { Link } from 'react-router-dom';
 
 const About = ({ showAboutPage, setShowAboutPage }) => {
 
@@ -10,23 +11,29 @@ const About = ({ showAboutPage, setShowAboutPage }) => {
       <Modal.Body>
         <h5>What is the goal of this app?</h5>
         <p>This app is made by passionate highline freestyle athletes that want to see the sport grow.
-            As more and more tricks are created every week it is easy to lose the overview.
-            Therefore we want to provide a source of the most common tricks/combos along with important information to help you learn them.
-            Also we want you to have a visual overview of which tricks/combos you are already consistent at and which of them you still need to work on.
+            As more and more tricks are created every week it is hard to keep the overview.
+            Here we want to show you the most common tricks/combos along with important information to help you learn them.
+            Setting the stick frequency on your tricks/combos allows you to have a visual overview of your arsenal.
             The feature that was the initial motivation for making this app is the random combo generator.
             It should help you trying out new combos that you otherwise wouldn't have tried.
             We want both beginners and advanced athletes to benefit from using this app.
-            </p>
+        </p>
         <h5>What are the next features we are working on?</h5>
         <p>We want to add a ComboMaker so you can create your own custom combos.</p>
         <h5>How can you contribute?</h5>
-        <p>If you want to have a specific feature or a bug fix in the future versions of this app please write Sebastian Egger via Facebook or Instagram.
-            If you are a developer yourself then don't hesitate to get involved in our repository:
+        <p>If you want to have a specific feature or a bug fix in the future versions of this app please write us via Facebook or Instagram.
+            If you are a developer yourself then don't hesitate to get involved in our repository: 
+            <br/>
+            <Link to={{ pathname: "https://github.com/bastislack/highline-freestyle" }} target="_blank">Github repository</Link>
         </p>
         <h5>How is your data handled?</h5>
         <p>As we are highliners we want to make this app work offline.
             Thus all your data is stored locally in your browser.
-            We use the localstorage and indexedDB API to save your data in your browser.
+            We use the&nbsp;
+             <Link to={{ pathname: "https://en.wikipedia.org/wiki/Web_storage" }} target="_blank">localstorage</Link> 
+            &nbsp;and&nbsp;
+             <Link to={{ pathname: "https://en.wikipedia.org/wiki/Indexed_Database_API" }} target="_blank">indexedDB</Link> 
+            &nbsp;API to save your data in your browser.
             We will never know about your data, only your browser knows about it!
         </p>
       </Modal.Body>
