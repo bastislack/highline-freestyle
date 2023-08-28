bastislack/highline-freestyle

commit 2382e36199444ba04a72ac66a6a0e29af0364122

db update fixes #194
Now only the explicit changes of the user are saved.
Before we saved a copy of the original trick in the `userTricks` table,
now this table only contains the explicit changed attributes.
Also for every trick query (single or multiple) the query has to merge
the `userTricks` table with the `predefinedTricks` one and by
encountering duplicates, keeping the user changed value.
This is done with the `mergeLists` function.

src/services/db.js
@@ -35,6 +35,20 @@ export default class Database {
       })
     });
 
+    this.db.version(7).stores().upgrade(tx => {
+      return tx.table("predefinedTricks").toArray().then(preTricks => {
+        return tx.table("userTricks").toCollection().modify(userTrick => {
+          const preTrick = preTricks.filter(trick => trick.id === userTrick.id)[0];
+          Object.keys(userTrick).forEach(key => {
+            if (key in preTrick && key !== "id" && userTrick[key] === preTrick[key]) {
+              console.log("deleting ", userTrick.id, key)
+              delete userTrick[key];
+            }
+          });
+        });
+      });
+    });
+
     this.db.on('ready', () => {
       // count the tricks in the database and populate it if its empty
       return this.db.versions.get("predefinedTricksVersion").then(ret => {
@@ -102,59 +116,86 @@ export default class Database {
     });
   };
 
+  // helper function to combine two lists, 
+  // where entries which share the same keys, get merged 
+  // and atributes of the first list are prefered
+  mergeLists = (listA, listB) => {
+    if (listA && !listB) return listA;
+    if (!listA && listB) return listB;
+    if (!listA && !listB) return null;
+    // Merge the two lists based on the common "id" attribute 
+    return listA.map((objA) => {
+      const objB = listB.find((objB) => objB.id === objA.id);
+      if (objB) {
+        // Combine the attributes of listA and listB, removing duplicates from listB
+        const objBcopy = { ...objB };
+        Object.keys(objA).forEach((key) => {
+          if (key in objB) {
+            delete objBcopy[key];
+          }
+        });
+        return { ...objA, ...objBcopy };
+      } else {
+        // If the id is unique keep it as is
+        return objA;
+      }
+    }).concat(listB.filter((objB) => !listA.some((objA) => objA.id === objB.id)))
+  };
+
   // get single trick by id
-  getTrick = (id) => this.db
-    .userTricks
-    .get(Number(id))
-    .then(userTrick => {
-      if (userTrick && !userTrick.deleted) return userTrick;
-      else return this.db.predefinedTricks.get(Number(id)
-    );
-  });
+  getTrick = (id) => this.db.userTricks.get(Number(id)).then(userTrick => {
+      return this.db.predefinedTricks.get(Number(id)).then(preTrick => {
+        if (!userTrick) return preTrick;
+        else return this.mergeLists([userTrick], [preTrick])[0];
+      });
+    });
 
   // get list of all tricks
+  //
+  // we combine all Tricks from both of the Tables, by their ids
+  // if an attribute is set by both tables, we take the attribute of the userTricks
   getAllTricks = () => {
     return this.db.userTricks.toArray().then((userTricks) => {
-      const userKeys = userTricks.map(trick => trick.id);
-      // query all only predefinedTricks which don't have the same id as the userTricks
-      // and concat these to the userTricks
-      // also filter out tricks which are marked deleted
-      return this.db
-        .predefinedTricks
-        .where("id")
-        .noneOf(userKeys)
-        .toArray()
-        .then(preTricks =>
-          preTricks
-            .concat(userTricks.filter(trick => !trick.deleted))
-            .sort((a,b) => a.id - b.id)
-        );
+      return this.db.predefinedTricks.toArray().then(preTricks => {
+        return this.mergeLists(userTricks, preTricks)
+            .filter(trick => !trick.deleted)
+            .sort((a,b) => a.id - b.id);
+        });
     });
   };
 
-  // get list of all tricks
+  // get list of tricks from ids
   getTricksByIds = (ids) => {
-    let result = [];
-    for (let i = 0; i < ids.length; i++) {
-      result.push(
-        this.db.userTricks.where("id").equals(ids[i]).last().then((userTrick) => {
-          if (userTrick && !userTrick.deleted) {
-            return userTrick;
-          } else {
-            return this.db.predefinedTricks.where("id").equals(ids[i]).last();
-          }
-        })
-      );
-    }
-    return result;
+    ids = ids.map(id => Number(id));
+    return this.db.userTricks.where("id").anyOf(ids).toArray().then(userTricks => {
+      return this.db.predefinedTricks.where("id").anyOf(ids).toArray().then(preTricks => {
+        return this.mergeLists(userTricks, preTricks);
+      });
+    });
   };
+
 
   getTricksByDiffAndByFreq = (diffLevels, stickFreqs) => {
-    return this.db.userTricks.toArray().then(userTricks => {
-      userTricks = userTricks.filter(trick => { return ((diffLevels.includes(trick.difficultyLevel) && stickFreqs.includes(trick.stickFrequency)) || trick.deleted);});
-      return this.db.predefinedTricks.where("difficultyLevel").anyOf(diffLevels).and(trick => stickFreqs.includes(trick.stickFrequency)).and(trick => !userTricks.map(trick => trick.id).includes(trick.id)).toArray().then(preTricks => preTricks.concat(userTricks.filter(trick => !trick.deleted)));
+    return this.getAllTricks().then(tricks => {
+      return tricks.filter(trick => {
+        return (diffLevels.includes(trick.difficultyLevel) && stickFreqs.includes(trick.stickFrequency));
+      });
     });
   };
+
+  //getTricksByDiffAndByFreq = (diffLevels, stickFreqs) => {
+  //  return this.db.userTricks.toArray().then(userTricks => {
+  //    userTricks = userTricks.filter(trick => { 
+  //      return ((diffLevels.includes(trick.difficultyLevel) && stickFreqs.includes(trick.stickFrequency)) || trick.deleted);
+  //    });
+  //    return this.db.predefinedTricks.where("difficultyLevel").anyOf(diffLevels)
+  //      .and(trick => stickFreqs.includes(trick.stickFrequency))
+  //      .and(trick => !userTricks.map(trick => trick.id).includes(trick.id))
+  //      .toArray().then(preTricks => {
+  //        preTricks.concat(userTricks.filter(trick => !trick.deleted))
+  //      });
+  //  });
+  //};
 
   // create or update userTrick
   saveTrick = (trick) => {
@@ -238,11 +279,10 @@ export default class Database {
   // fill a combo, which has only ids as tricks with the full tricks
   // (containing id, name, level, etc.)
   fillComboWithTricks = (combo) => {
-    return Promise.all(this.getTricksByIds(combo.tricks))
-      .then(tricksInCombo => {
-        combo.tricks = tricksInCombo;
-        return combo;
-      });
+    return this.getTricksByIds(combo.tricks).then(tricksInCombo => {
+      combo.tricks = tricksInCombo;
+      return combo;
+    });
   };
 
   // get list of all combos

src/components/tricks/PostTrick.js
@@ -73,6 +73,18 @@ const PostTrick = () => {
       tips: tips,
       stickFrequency: stickFrequency
     };
+
+    // removes all attributes,which were not set or modified
+    if (trick.alias == "" || !trick.alias || preTrick && trick.alias === preTrick.alias){ delete trick.alias }
+    if (trick.technicalName == "" || !trick.technicalName || preTrick && trick.technicalName === preTrick.technicalName){ delete trick.technicalName }
+    if (trick.establishedBy == "" || !trick.establishedBy || preTrick && trick.establishedBy === preTrick.establishedBy){ delete trick.establishedBy }
+    if (trick.yearEstablished == "" || !trick.yearEstablished || preTrick && trick.yearEstablished === preTrick.yearEstablished){ delete trick.yearEstablished }
+    if (trick.linkToVideo == "" || !trick.linkToVideo || preTrick && trick.linkToVideo === preTrick.linkToVideo){ delete trick.linkToVideo }
+    if (trick.startPos == "" || !trick.startPos || preTrick && trick.startPos === preTrick.startPos){ delete trick.startPos }
+    if (trick.endPos == "" || !trick.endPos || preTrick && trick.endPos === preTrick.endPos){ delete trick.endPos }
+    if (trick.difficultyLevel == "" || !trick.difficultyLevel || preTrick && trick.difficultyLevel === preTrick.difficultyLevel){ delete trick.difficultyLevel }
+    if (trick.description == "" || !trick.description || preTrick && trick.description === preTrick.description){ delete trick.description }
+    if (trick.tips == "" || !trick.tips || preTrick && trick.tips === preTrick.tips){ delete trick.tips }
     
     db.saveTrick(trick)
     .then(() => {

src/components/tricks/TrickDetails.js
@@ -41,8 +41,10 @@ const TrickDetails = () => {
 
   const selectFreq = (e) => {
     const newFreq = Number(e.target.value);
-    trick.stickFrequency = newFreq;
-    db.saveTrick(trick).then(res => {
+    let modifiedTrick = Object();
+    modifiedTrick.id = trick.id;
+    modifiedTrick.stickFrequency = newFreq;
+    db.saveTrick(modifiedTrick).then(res => {
       console.log("changed stickFrequency");
     }).catch(e => {
       console.log(e);

commit 33e31cbea1a5c5da072e379d8def01994bfeab5b

modify userTrick stickFrequency bug
Commit 2382e36 introduced a bug, which deleted all attributes
(Name, Alias, Description, ...) of a userTrick when changing the
`stickFrequency` over the `FreqList` (the colorful radio-boxes).

This introduces a new method to modify only the `stickFrequency`

src/components/tricks/TrickDetails.jsx
@@ -40,10 +40,7 @@ const TrickDetails = () => {
 
   const selectFreq = (e) => {
     const newFreq = Number(e.target.value);
-    let modifiedTrick = Object();
-    modifiedTrick.id = trick.id;
-    modifiedTrick.stickFrequency = newFreq;
-    db.saveTrick(modifiedTrick).catch(e => {
+    db.changeTrickStickFrequency(trick.id, newFreq).catch(e => {
       console.warn(e);
     });
   }

src/services/db.js
@@ -230,6 +230,13 @@ export default class Database {
     }
   };
 
+  // updatesonly the stickFrequency if a trick with the id exists in the userTricks tabel, ohterwise creates a new entry
+  changeTrickStickFrequency = (trickId, newFrequency) => {
+    return this.db.userTricks.update(trickId, {stickFrequency: newFrequency}).then((worked) => {
+      if (!worked) return this.db.userTricks.put(Object({id: trickId, stickFrequency: newFrequency}));
+    });
+  };
+
   // delete trick
   deleteTrick = (id) => this.db.userTricks.put({"id": Number(id), deleted: true});
 