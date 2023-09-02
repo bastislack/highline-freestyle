9d313aa54b69abf9ca17e8a909e146dcf0e1ac50
Waldemar Lehner <mail@waldemarlehner.de>
2023-09-02 15:47:48 +0200

    Add new route as an example containing partial translations with EN fallback

---
+++ src/i18n/notFound.ts
@@ -0,0 +1,6 @@
+import de from "./notFound.de.json"
+import en from "./notFound.en.json"
+
+export default {
+  en, de
+}
---
+++ src/routes/NotFoundRoute.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import DefaultLayout from '../layouts/DefaultLayout.vue';
+import { useI18n } from 'vue-i18n'
+
+import messages from "../i18n/notFound"
+import { useRoute } from 'vue-router';
+
+const i18n = useI18n({
+  messages,
+  useScope: "local"
+})  
+
+const route = useRoute()
+
+const {t} = i18n;
+
+</script>
+
+
+<template>
+  <DefaultLayout :flex-spacer="false">
+    <div class="flex flex-1 flex-col justify-center  self-stretch ">
+      <div class="flex flex-row justify-center">
+        <div class="flex flex-col">
+          <h1 class="text-5xl font-bold dark:text-white transition-all duration-200 hover:-translate-y-3">{{ t('heading') }}</h1>
+          <p class="text-sm">{{ route.fullPath }}</p>
+          <p class="text-md dark:text-neutral-300 text-neutral-400">
+            {{ t('body') }}
+          </p>
+          <RouterLink class="p-2 rounded-lg dark:border-neutral-400 border-primary border text-lg mt-4 dark:text-neutral-400 text-primary hover:text-white hover:dark:bg-slate-800 hover:border-transparent dark:hover:border-transparent hover:bg-primary duration-150 " to="/">
+            {{  t("backToIndex") }}
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+  </DefaultLayout>
+
+</template>
