939bd3fabf5c6dc1a624c7eec318e2a05d73ba4e
Sebastian Egger <sebastian.egger@zollsoft.de>
2023-10-26 19:08:38 +0200

    Add PWA Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'logo192.png'],
+      manifest: 
+        {
+        "short_name": "Freestyle",
+        "name": "Highline Freestyle",
+        "icons": [
+          {
+            "src": "favicon.ico",
+            "type": "image/x-icon"
+          },
+          {
+            "src": "logo192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "logo512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+          }
+        ],
+        "start_url": ".",
+        "display": "standalone",
+        "theme_color": "#000000",
+        "background_color": "#ffffff",
+        "orientation": "portrait-primary"
+      },
+    }),
+  ],
 })
