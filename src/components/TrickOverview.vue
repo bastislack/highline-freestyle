4b499ebccb86faaeb3b1e047b27d98e0c80f1aab
Waldemar Lehner <mail@waldemarlehner.de>
2023-10-29 16:04:40 +0100

    Fixup TrickOverview

--- src/components/TrickOverview.vue
+++ src/components/TrickOverview.vue
@@ -1,25 +1,27 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
 import { Trick } from '../util/trick';
 import { tricks } from '../util/tricks';
 
 const allTricks: Trick[] = tricks
 
-const tricksByDifficulty: Map<number, Trick[]> = new Map()
+const tricksByDifficulty: Record<number, Trick[]> = {}
 
-onMounted(() => {
-  allTricks.forEach(trick => {
-    if (tricksByDifficulty.has(trick.difficultyLevel)) {
-      tricksByDifficulty.get(trick.difficultyLevel)?.push(trick)
-    } else {
-      tricksByDifficulty.set(trick.difficultyLevel, [trick])
-    }
-  })
 
-  tricksByDifficulty.forEach((tricks) => {
-    tricks.sort(compareTrickNames)
-  })
+allTricks.forEach(trick => {
+  const difficultyLevel = trick.difficultyLevel;
+  if(!tricksByDifficulty[difficultyLevel]) {
+    tricksByDifficulty[difficultyLevel] = []
+  }
+
+  tricksByDifficulty[difficultyLevel].push(trick)
 })
+
+Object.keys(tricksByDifficulty).forEach( 
+  e => tricksByDifficulty[e as any] = tricksByDifficulty[e as any].sort(compareTrickNames)
+);
+
+console.log(tricksByDifficulty)
+
 
 function compareTrickNames(a: Trick, b: Trick) {
   if (a.alias && b.alias) {
@@ -37,13 +39,13 @@ function compareTrickNames(a: Trick, b: Trick) {
 
 
 <template>
-  <div v-for="level in tricksByDifficulty.size - 1" :key="level">
+  <div v-for="level in Object.keys(tricksByDifficulty)" :key="level">
     <h2 class="text-2xl font-bold py-2 text-center bg-light-gray">
       Level {{ level }}
     </h2>
     <hr class="drop-shadow border-dark-gray">
     <div class="p-4 grid grid-cols-3 gap-3">
-      <div v-for="trick in tricksByDifficulty.get(level)!" :key="trick.id"
+      <div v-for="trick in tricksByDifficulty[level as any]!" :key="trick.id"
         class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
         <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
           {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
@@ -57,7 +59,7 @@ function compareTrickNames(a: Trick, b: Trick) {
   </h2>
   <hr class="drop-shadow border-dark-gray">
   <div class="p-4 grid grid-cols-3 gap-3">
-    <div v-for="trick in tricksByDifficulty.get(-1)!" :key="trick.id"
+    <div v-for="trick in tricksByDifficulty['-1'] ?? []" :key="trick.id"
       class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
       <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
         {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
