691858e8594178feec463f69040853330cd572ea
Sebastian Egger <sebastian.egger@zollsoft.de>
2023-10-30 15:23:45 +0100

    Make trick overview responsive

--- src/components/TrickOverview.vue
+++ src/components/TrickOverview.vue
@@ -9,14 +9,14 @@ const tricksByDifficulty: Record<number, Trick[]> = {}
 
 allTricks.forEach(trick => {
   const difficultyLevel = trick.difficultyLevel;
-  if(!tricksByDifficulty[difficultyLevel]) {
+  if (!tricksByDifficulty[difficultyLevel]) {
     tricksByDifficulty[difficultyLevel] = []
   }
 
   tricksByDifficulty[difficultyLevel].push(trick)
 })
 
-Object.keys(tricksByDifficulty).forEach( 
+Object.keys(tricksByDifficulty).forEach(
   e => tricksByDifficulty[e as any] = tricksByDifficulty[e as any].sort(compareTrickNames)
 );
 
@@ -39,12 +39,12 @@ function compareTrickNames(a: Trick, b: Trick) {
 
 
 <template>
-  <div v-for="level in Object.keys(tricksByDifficulty)" :key="level">
+  <div v-for="level in Object.keys(tricksByDifficulty).length - 1" :key="level">
     <h2 class="text-2xl font-bold py-2 text-center bg-light-gray">
       Level {{ level }}
     </h2>
     <hr class="drop-shadow border-dark-gray">
-    <div class="p-4 grid grid-cols-3 gap-3">
+    <div class="p-4 grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-3">
       <div v-for="trick in tricksByDifficulty[level as any]!" :key="trick.id"
         class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
         <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
@@ -58,7 +58,7 @@ function compareTrickNames(a: Trick, b: Trick) {
     To Be Determined
   </h2>
   <hr class="drop-shadow border-dark-gray">
-  <div class="p-4 grid grid-cols-3 gap-3">
+  <div class="p-4 grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-3">
     <div v-for="trick in tricksByDifficulty['-1'] ?? []" :key="trick.id"
       class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
       <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
