2054084e7cd55b76ee7d06fc27fab50945d7b1f1
Waldemar Lehner <mail@waldemarlehner.de>
2023-10-31 22:58:37 +0100

    Hook up DB to Trick Overview Component, add Loading Component, make Trick Overview be async

--- src/components/TrickOverview.vue
+++ src/components/TrickOverview.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
-import { Trick } from '../util/trick';
-import { tricks } from '../util/tricks';
+// !Warning! This Component has an async Setup. As such it MUST be encapsulated in a Suspense-Wrapper!
 
-const allTricks: Trick[] = tricks
+import type { Trick } from '../lib/database/daos/trick';
+
+
+const allTricks = await (await import('../lib/database')).tricksDao.getAll()
 
 const tricksByDifficulty: Record<number, Trick[]> = {}
 
@@ -39,13 +41,13 @@ function compareTrickNames(a: Trick, b: Trick) {
 
 
 <template>
-  <div v-for="level in Object.keys(tricksByDifficulty).length - 1" :key="level">
+  <div v-for="level in Object.keys(tricksByDifficulty)" :key="level">
     <h2 class="text-2xl font-bold py-2 text-center bg-light-gray">
       Level {{ level }}
     </h2>
     <hr class="drop-shadow border-dark-gray">
     <div class="p-4 flex xs:grid flex-col  xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-2">
-      <div v-for="trick in tricksByDifficulty[level as any]!" :key="trick.id"
+      <div v-for="trick in tricksByDifficulty[level as any]!" :key="trick.technicalName"
         class="p-2 border rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
         <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
           {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
@@ -59,7 +61,7 @@ function compareTrickNames(a: Trick, b: Trick) {
   </h2>
   <hr class="drop-shadow border-dark-gray">
   <div class="p-4 grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-3">
-    <div v-for="trick in tricksByDifficulty['-1'] ?? []" :key="trick.id"
+    <div v-for="trick in tricksByDifficulty['-1'] ?? []" :key="trick.technicalName"
       class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
       <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
         {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
--- src/routes/IndexRoute.vue
+++ src/routes/IndexRoute.vue
@@ -1,12 +1,18 @@
 <script lang="ts" setup>
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 import TrickOverview from '../components/TrickOverview.vue';
+import LoadingSpinner from '../components/LoadingSpinner.vue';
 
 </script>
 
 
 <template>
-  <DefaultLayout :flex-spacer="false">
-    <TrickOverview />
+  <DefaultLayout :flex-spacer="true">
+    <Suspense>
+      <TrickOverview />
+      <template #fallback>
+        <LoadingSpinner/>
+      </template>
+    </Suspense>
   </DefaultLayout>
 </template>
