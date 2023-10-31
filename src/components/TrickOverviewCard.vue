87bb1b061a5369b982ec79ce181501c05f6218e4
Waldemar Lehner <mail@waldemarlehner.de>
2023-10-31 23:04:01 +0100

    Extract Card from Trick List into separate Component

---
+++ src/components/TrickOverviewCard.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { type Trick } from '../lib/database/daos/trick';
+
+// This component defines a single card inside the Trick Overview
+interface Props {
+  trick: Trick
+}
+
+const {trick} = defineProps<Props>();
+
+</script>
+
+<template>
+  <div
+    class="p-2 border rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
+    <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
+      {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
+    </p>
+  </div>
+</template>
