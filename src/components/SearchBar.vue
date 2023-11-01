781da4e5c038dc2e2fa6200f5a9388f10450c1a9
Sebastian Egger <sebastian.egger@zollsoft.de>
2023-11-01 19:40:06 +0100

    initial attempt search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Icon } from '@iconify/vue';
+
+const searchText = ref('')
+
+</script>
+
+
+<template>
+  <input v-model="searchText" type="text"
+    class="m-2 mt-4 border-2 border-dark-gray bg-light-gray-250 h-10 px-5 rounded-lg text-sm focus:outline-none"
+    placeholder="Search trick" @input="$emit('update-search-text', searchText)">
+</template>
--- src/components/TrickOverview.vue
+++ src/components/TrickOverview.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { Trick } from '../util/trick';
 import { tricks } from '../util/tricks';
+import SearchBar from './SearchBar.vue'
 
 const allTricks: Trick[] = tricks
 
+const filteredTricks = ref([] as Trick[])
+
 const tricksByDifficulty: Record<number, Trick[]> = {}
 
+const isSearchActive = ref(false)
 
 allTricks.forEach(trick => {
   const difficultyLevel = trick.difficultyLevel;
@@ -23,7 +28,7 @@ Object.keys(tricksByDifficulty).forEach(
 console.log(tricksByDifficulty)
 
 
-function compareTrickNames(a: Trick, b: Trick) {
+function compareTrickNames(a: Trick, b: Trick): number {
   if (a.alias && b.alias) {
     return a.alias.localeCompare(b.alias)
   } else if (a.alias) {
@@ -35,35 +40,67 @@ function compareTrickNames(a: Trick, b: Trick) {
   }
 }
 
+function updateSearch(searchText: string): void {
+  searchText = searchText.trim()
+  if (searchText) {
+    filterTricks(searchText)
+    isSearchActive.value = true
+  } else {
+    filteredTricks.value = []
+    isSearchActive.value = false
+  }
+}
+
+function filterTricks(searchText: string): void {
+  filteredTricks.value = allTricks.filter(trick => {
+    return trick.alias?.toLowerCase().includes(searchText.toLowerCase()) ||
+      trick.technicalName.toLowerCase().includes(searchText.toLowerCase())
+  })
+}
+
 </script>
 
 
 <template>
-  <div v-for="level in Object.keys(tricksByDifficulty).length - 1" :key="level">
+  <SearchBar @update-search-text="updateSearch" />
+  <div v-if="!isSearchActive">
+    <div v-for="level in Object.keys(tricksByDifficulty).length - 1" :key="level">
+      <hr class="border-dark-gray">
+      <h2 class="text-2xl font-bold py-2 text-center bg-light-gray">
+        Level {{ level }}
+      </h2>
+      <hr class="drop-shadow border-dark-gray">
+      <div class="p-4 grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-3">
+        <div v-for="trick in tricksByDifficulty[level as any]!" :key="trick.id"
+          class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
+          <p class="text-dark-gray text-center line-clamp-3 text-base/5 break-words">
+            {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <hr class="border-dark-gray">
     <h2 class="text-2xl font-bold py-2 text-center bg-light-gray">
-      Level {{ level }}
+      To Be Determined
     </h2>
     <hr class="drop-shadow border-dark-gray">
     <div class="p-4 grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-3">
-      <div v-for="trick in tricksByDifficulty[level as any]!" :key="trick.id"
+      <div v-for="trick in tricksByDifficulty['-1'] ?? []" :key="trick.id"
         class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
-        <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
+        <p class="text-dark-gray text-center line-clamp-3 text-base/5 break-words">
           {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
         </p>
       </div>
     </div>
-    <hr class="border-dark-gray">
   </div>
-  <h2 class="text-2xl font-bold py-2 text-center bg-light-gray">
-    To Be Determined
-  </h2>
-  <hr class="drop-shadow border-dark-gray">
-  <div class="p-4 grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-3">
-    <div v-for="trick in tricksByDifficulty['-1'] ?? []" :key="trick.id"
-      class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
-      <p class="text-dark-gray text-center line-clamp-3 text-base/5 ">
-        {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
-      </p>
+  <div v-else>
+    <div class="p-4 grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-3">
+      <div v-for="trick in filteredTricks" :key="trick.id"
+        class="p-2 border-2 rounded-md border-dark-gray bg-light-gray-250 flex items-center justify-center container h-20 drop-shadow">
+        <p class="text-dark-gray text-center line-clamp-3 text-base/5 break-words">
+          {{ trick.alias && trick.alias.length > 0 ? trick.alias : trick.technicalName }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
