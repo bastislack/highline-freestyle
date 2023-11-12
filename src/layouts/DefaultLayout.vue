c101badb520bd11792190edf05f08863c14a4112
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2023-11-12 18:07:28 +0100

    Update to better system of hiding components

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -7,13 +7,13 @@ import NavbarBottom from '../components/navbars/NavbarBottom.vue';
 <template>
   <div class="flex flex-col min-h-screen bg-white text-stone-900 m-0 p-0">
     <div class="flex flex-row m-0 p-0">
-      <div class="collapse lg:visible">
+      <div class="hidden lg:block">
         <NavbarSide />
         <div class="w-56"/>
       </div>
       <div>
         <slot />
-        <div class="visible lg:collapse">
+        <div class="block lg:hidden">
           <div class="h-16 w-full" />
           <NavbarBottom />
         </div>
