a42ac99118748eb73fbd835607028d9ca6b483ae
Waldemar Lehner <mail@waldemarlehner.de>
2023-11-25 20:02:34 +0100

    Add Eslint and Prettier with initial configs, add max line length to editorconfig

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false
+};
+
+// eslint-disable-next-line no-undef
+module.exports = config;
