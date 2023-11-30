58d8ea897a18b00083d7b64a981772542a5a355e
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2023-11-30 17:11:04 +0100

    refactor(i18n): clean i18n folder structure (#312)
    - Clean i18n folder structure
    - Add translations of "notFound" into Spanish and French

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,9 @@
 import en from './index.en.json';
-import fr from './index.fr.json';
 import es from './index.es.json';
+import fr from './index.fr.json';
 
 export default {
   en,
-  fr,
   es,
+  fr,
 };
--- src/i18n/notFound.ts
+++ src/i18n/notFound.ts
@@ -1,5 +1,9 @@
 import en from './notFound.en.json';
+import es from './notFound.es.json';
+import fr from './notFound.fr.json';
 
 export default {
   en,
+  es,
+  fr,
 };
