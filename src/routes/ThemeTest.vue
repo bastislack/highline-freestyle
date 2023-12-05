fa066585652b82199aa1e3d756a6cc0ec342fb5b
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2023-12-05 14:05:08 +0100

    feat(ui): Add page for testing themes

---
+++ src/routes/ThemeTest.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="bg-background text-foreground">
+    <!--
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center bg-blue-50">50</div>
+      <div class="w-24 py-3 rounded-md text-center bg-blue-100">100</div>
+      <div class="w-24 py-3 rounded-md text-center bg-blue-300">300</div>
+      <div class="w-24 py-3 rounded-md text-center bg-blue-500">blue</div>
+      <div class="w-24 py-3 rounded-md text-center bg-blue-700">700</div>
+      <div class="w-24 py-3 rounded-md text-center bg-blue-900">900</div>
+      <div class="w-24 py-3 rounded-md text-center bg-blue-950">950</div>
+    </div>
+    -->
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center text-primary-foreground bg-primary-50">50</div>
+      <div class="w-24 py-3 rounded-md text-center text-primary-foreground bg-primary-100">100</div>
+      <div class="w-24 py-3 rounded-md text-center text-primary-foreground bg-primary-300">300</div>
+      <div class="w-24 py-3 rounded-md text-center text-primary-foreground bg-primary">primary</div>
+      <div class="w-24 py-3 rounded-md text-center text-primary-foreground bg-primary-700">700</div>
+      <div class="w-24 py-3 rounded-md text-center text-primary-foreground bg-primary-900">900</div>
+      <div class="w-24 py-3 rounded-md text-center text-primary-foreground bg-primary-950">950</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center text-secondary-foreground bg-secondary">
+        secondary
+      </div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center text-accent-foreground bg-accent">accent</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center text-muted-foreground bg-muted">muted</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 mt-2 mb-5 mx-2">
+      <div class="w-24 py-3 rounded-md text-center text-destructive-foreground bg-destructive-50">
+        50
+      </div>
+      <div class="w-24 py-3 rounded-md text-center text-destructive-foreground bg-destructive-100">
+        100
+      </div>
+      <div class="w-24 py-3 rounded-md text-center text-destructive-foreground bg-destructive-300">
+        300
+      </div>
+      <div class="w-24 py-3 rounded-md text-center text-destructive-foreground bg-destructive">
+        destruct.
+      </div>
+      <div class="w-24 py-3 rounded-md text-center text-destructive-foreground bg-destructive-700">
+        700
+      </div>
+      <div class="w-24 py-3 rounded-md text-center text-destructive-foreground bg-destructive-900">
+        900
+      </div>
+      <div class="w-24 py-3 rounded-md text-center text-destructive-foreground bg-destructive-950">
+        950
+      </div>
+    </div>
+
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center bg-skill1">skill1</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center bg-skill2">skill2</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center bg-skill3">skill3</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center bg-skill4">skill4</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center bg-skill5">skill5</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center bg-skill6">skill6</div>
+    </div>
+    <div class="w-fill flex flex-row justify-center space-x-2 my-2 mx-2">
+      <div class="w-24 py-3 rounded-md text-center bg-skill7">skill7</div>
+    </div>
+  </div>
+</template>
