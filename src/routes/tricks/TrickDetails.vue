b748100ee51ccd0f573b8a663999c7585fd32835
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2023-12-15 20:24:12 +0100

    Get url params and db to work

--- src/routes/tricks/TrickDetails.vue
+++ src/routes/tricks/TrickDetails.vue
@@ -1,37 +1,75 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router';
+import { z } from 'zod';
+import { onMounted, ref, watch } from 'vue';
+
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import { DbTricksTableZod } from '@/lib/database/schemas/Version1Schema';
-
-//import { tricksDao } from '@/lib/database';
+import { tricksDao, Trick } from '@/lib/database';
 
 const route = useRoute();
 
-// Get and validate id
-const rawId = Number(route.params.id);
-const idResult = DbTricksTableZod._def.shape().id.safeParse(rawId);
-const error_ = !idResult.success;
-let errorMsg = '';
-let id = 0;
-if (!idResult.success) {
-  errorMsg = 'Invalid ID';
-} else {
-  id = idResult.data;
+let inputError: boolean = false;
+let errorMsg: string[] = [];
+
+function parseAndValidateId(): z.SafeParseReturnType<number, number> {
+  const idRaw = Number(route.params.id);
+  const idResult = DbTricksTableZod._def.shape().id.safeParse(idRaw);
+  return idResult;
 }
 
-// Get and validate status
+function parseAndValidateStatus(): z.SafeParseReturnType<
+  'official' | 'userDefined' | 'archived',
+  'official' | 'userDefined' | 'archived'
+> {
+  const statusRaw = route.params.status;
+  const statusResult = DbTricksTableZod._def.shape().trickStatus.safeParse(statusRaw);
+  //let status: DbTricks["trickStatus"] = "official";
+  return statusResult;
+}
+
+async function getTrick(): Promise<Trick> {
+  const idResult: z.SafeParseReturnType<number, number> = parseAndValidateId();
+  if (!idResult.success) {
+    return undefined;
+  }
+
+  const statusResult = parseAndValidateStatus();
+  if (!statusResult.success) {
+    return undefined;
+  }
+
+  return await tricksDao.getById(idResult.data, statusResult.data);
+}
+
+let trick = ref<Trick>(undefined);
+
+// Load content initially
+onMounted(async () => {
+  trick.value = await getTrick();
+});
+
+// Update content when URL changes
+watch(route, async () => {
+  trick.value = await getTrick();
+});
 </script>
 
 <template>
   <DefaultLayout>
-    <div v-if="error_">
-      <h3>Error</h3>
-      <div>{{ errorMsg }}</div>
-    </div>
-    <div v-else>
-      <h3>Success</h3>
-      <div>{{ $route.params.status }}</div>
-      <div>{{ '' + id }}</div>
+    <div class="m-3 lg:m-6">
+      <div v-if="inputError">
+        <h3 class="text-2xl">Error</h3>
+        <div>{{ errorMsg }}</div>
+      </div>
+      <div v-else>
+        <h3 class="text-2xl">Success</h3>
+        <!--<div>{{ status }}</div>-->
+        <!--<div>{{ '' + id }}</div>-->
+        <div v-if="trick !== undefined">
+          {{ trick.technicalName }}
+        </div>
+      </div>
     </div>
   </DefaultLayout>
 </template>
