7827ecdd05650f14085b860a83e0d7bf70804a78
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2024-01-03 09:21:37 +0100

    fix(ui): Change dropdown separator color to secondary

--- src/components/ui/dropdown-menu/DropdownMenuSeparator.vue
+++ src/components/ui/dropdown-menu/DropdownMenuSeparator.vue
@@ -5,5 +5,5 @@ const props = defineProps<DropdownMenuSeparatorProps>();
 </script>
 
 <template>
-  <DropdownMenuSeparator v-bind="props" class="-mx-1 my-1 h-px bg-muted" />
+  <DropdownMenuSeparator v-bind="props" class="-mx-1 my-1 h-px bg-secondary" />
 </template>
