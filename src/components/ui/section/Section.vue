62312efd2063d222114b804e4997cbc00ca4e0e7
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2024-01-19 15:04:53 +0100

    feat(ui): improve layout in DefaultLayout and with Section component

---
+++ src/components/ui/section/Section.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="w-full flex flex-col items-center p-3 md:p-5 lg:p-6 xl:px-12">
+    <div class="w-full max-w-4xl">
+      <slot />
+    </div>
+  </section>
+</template>
