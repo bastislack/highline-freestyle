cb251efa7818483cb893ee0a449a27ec3947497a
Julian Dietzel <1839550-JulianDietzel@users.noreply.gitlab.com>
2024-04-16 19:58:31 +0200

    Add translation to settings page

--- src/i18n/settings.ts
+++ src/i18n/settings.ts
@@ -1,5 +1,9 @@
 import en from './settings/settings.en.json';
+import es from './settings/settings.es.json';
+import fr from './settings/settings.fr.json';
 
 export default {
   en,
+  es,
+  fr,
 };
