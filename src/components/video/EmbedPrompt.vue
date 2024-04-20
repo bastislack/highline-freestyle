6796602e73ef1c085dc094055a7a334bebb45106
Waldemar Lehner <mail@waldemarlehner.de>
2024-04-20 19:41:59 +0200

    nit: use lookup record instead of ternary operator to determine i18n key

--- src/components/video/EmbedPrompt.vue
+++ src/components/video/EmbedPrompt.vue
@@ -15,6 +15,16 @@ const i18n = useI18n({
 });
 const { t } = i18n;
 
+const buttonTranslationKey: Record<EmbedSite, string> = {
+  INSTAGRAM: 'embedPermissions.instagram.onlyInstagram',
+  YOUTUBE: 'embedPermissions.youtube.onlyYouTube',
+};
+
+const questionTranslationKey: Record<EmbedSite, string> = {
+  INSTAGRAM: 'embedPermissions.instagram.question',
+  YOUTUBE: 'embedPermissions.youtube.question',
+};
+
 function allowYouTubeAndInstagramEmbeds() {
   setEmbedPreference('YOUTUBE', true);
   setEmbedPreference('INSTAGRAM', true);
@@ -26,20 +36,12 @@ function allowYouTubeAndInstagramEmbeds() {
     <div class="flex flex-col gap-2 align-middle w-full text-sm">
       <div class="font-medium text-lg">{{ t('embedPermissions.heading') }}</div>
       <div>
-        {{
-          site === 'YOUTUBE'
-            ? t('embedPermissions.youtube.question')
-            : t('embedPermissions.instagram.question')
-        }}
+        {{ t(questionTranslationKey[site]) }}
       </div>
       <div class="text-muted-foreground">{{ t('embedPermissions.details') }}</div>
       <div class="flex flex-row gap-2">
         <Button size="sm" variant="outline" :onclick="() => setEmbedPreference(site, true)">
-          {{
-            site === 'YOUTUBE'
-              ? t('embedPermissions.youtube.onlyYouTube')
-              : t('embedPermissions.instagram.onlyInstagram')
-          }}
+          {{ t(buttonTranslationKey[site]) }}
         </Button>
         <Button size="sm" variant="outline" :onclick="allowYouTubeAndInstagramEmbeds">
           {{ t('embedPermissions.youtubeAndInstagram') }}
