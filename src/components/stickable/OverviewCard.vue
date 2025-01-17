f02acb4400f8e1f5e3c2b7803a290fdc13f4656f
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2025-01-17 13:34:26 +0100

    Functional rudamentary search and output

--- src/components/stickable/OverviewCard.vue
+++ src/components/stickable/OverviewCard.vue
@@ -9,12 +9,13 @@ const props = defineProps<{
   title: string;
   status: StickableStatus;
   linkToDetails: string;
-  stickFrequency: number;
+  stickFrequency?: number;
   isFavorite: boolean;
   isNew: boolean;
 }>();
 
 const borderClass = computed<string>(() => {
+  let stickFrequency = Math.max(0, Math.min(props.stickFrequency ?? 0, 7));
   return [
     'border-border',
     'border-skill1',
@@ -24,23 +25,34 @@ const borderClass = computed<string>(() => {
     'border-skill5',
     'border-skill6',
     'border-skill7',
-  ][Math.max(0, Math.min(props.stickFrequency, 7))];
+  ][stickFrequency];
 });
 </script>
 
 <template>
   <RouterLink
     :to="linkToDetails"
-    class="p-1 rounded-sm border flex flex-col items-center text-center"
+    class="p-1 rounded-sm border flex flex-col items-center justify-center text-center"
     :class="borderClass"
   >
     <div>{{ title }}</div>
-    <div v-if="isNew || status === 'userDefined'" class="flex flex-row gap-1">
-      <Badge v-if="status === 'userDefined'">
-        <Icon icon="ic:round-person" />
+    <div
+      v-if="isNew || status === 'userDefined'"
+      class="mt-1 flex flex-row justify-center gap-1 flex-wrap"
+    >
+      <Badge v-if="status === 'userDefined'" class="py-0 px-2" variant="secondary">
+        <Icon icon="ic:round-person" class="mr-1" />
         Custom
       </Badge>
-      <Badge v-if="isNew">New</Badge>
+      <Badge v-else-if="status === 'archived'" class="py-0 px-2" variant="destructive">
+        <Icon icon="ic:baseline-archive" class="mr-1" />
+        Archived
+      </Badge>
+
+      <Badge v-if="isNew" class="py-0 px-2" variant="secondary">
+        <Icon icon="ic:baseline-filter-vintage" class="mr-1" />
+        New
+      </Badge>
     </div>
   </RouterLink>
 </template>
--- src/routes/tricks/TrickList.vue
+++ src/routes/tricks/TrickList.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
-//import { tricksDao } from '@/lib/database';
-//import { Trick } from '@/lib/database/daos/trick';
+import { tricksDao } from '@/lib/database';
+import { Trick } from '@/lib/database/daos/trick';
 import { PrimaryKey } from '@/lib/utils';
-import { ref } from 'vue';
-//import { isStickableNew } from '@/util/misc';
+import { ref, watchEffect } from 'vue';
+import { isStickableNew } from '@/util/misc';
+import OverviewCard from '@/components/stickable/OverviewCard.vue';
 
 type SearchResult = SearchSection[];
 
@@ -21,63 +22,75 @@ type SearchItem = {
   isFavorite: boolean;
 };
 
-/*
 function sortByPrimaryKeyCondition(a: Trick, b: Trick): number {
-  const statusOrder = ["official", "userDefined", "archived"]
-  const statusDiff = statusOrder.indexOf(a.primaryKey[1]) - statusOrder.indexOf(b.primaryKey[1])
+  const statusOrder = ['official', 'userDefined', 'archived'];
+  const statusDiff = statusOrder.indexOf(a.primaryKey[1]) - statusOrder.indexOf(b.primaryKey[1]);
 
   if (statusDiff !== 0) {
-    return statusDiff
+    return statusDiff;
   }
 
-  return a.primaryKey[0] - b.primaryKey[0]
+  return a.primaryKey[0] - b.primaryKey[0];
 }
 
 function sortByDifficultyAscendingCondition(a: Trick, b: Trick): number {
-  let comp = 0
+  let comp = 0;
   if (a.difficultyLevel !== undefined && b.difficultyLevel !== undefined) {
-    comp = a.difficultyLevel - b.difficultyLevel
-  }
-  else if (a.difficultyLevel === undefined) {
-    comp = (b.difficultyLevel === undefined) ? 0 : 1
-  }
-  else {
-    comp = -1
+    comp = a.difficultyLevel - b.difficultyLevel;
+  } else if (a.difficultyLevel === undefined) {
+    comp = b.difficultyLevel === undefined ? 0 : 1;
+  } else {
+    comp = -1;
   }
 
   if (comp == 0) {
-    return sortByPrimaryKeyCondition(a, b)
+    return sortByPrimaryKeyCondition(a, b);
   }
 
-  return comp
+  return comp;
 }
 
-function getParameter(trick: Trick, parameter: "difficulty"): string {
-  if (parameter === "difficulty") {
-    return trick.difficultyLevel ? `Difficulty ${trick.difficultyLevel}` : "Not determined"
+function getParameter(trick: Trick, parameter: 'difficulty'): string {
+  if (parameter === 'difficulty') {
+    return trick.difficultyLevel ? `Difficulty ${trick.difficultyLevel}` : 'Not determined';
   }
-  return ""
+  return '';
 }
 
-async function search(): SearchResult {
-  const allTricks = (await tricksDao.getAll()).sort((a, b) => sortByDifficultyAscendingCondition(a, b))
-  let currentGroup = getParameter(allTricks[0], "difficulty")
-  let result: SearchResult = [{title: currentGroup, items: []}]
+async function search(): Promise<SearchResult> {
+  const allTricks = (await tricksDao.getAll()).sort((a, b) =>
+    sortByDifficultyAscendingCondition(a, b)
+  );
+  let currentGroup = getParameter(allTricks[0], 'difficulty');
+  let result: SearchResult = [{ title: currentGroup, items: [] }];
   for (let trick of allTricks) {
     let searchItem: SearchItem = {
       name: trick.alias ?? trick.technicalName,
-      primaryKey: trick.primaryKey,
+      primaryKey: [trick.primaryKey[0], trick.primaryKey[1]],
       stickFrequency: trick.stickFrequency,
       isFavorite: trick.isFavourite,
-      isNew: isStickableNew(trick.dateAddedEpoch)
+      isNew: isStickableNew(trick.dateAddedEpoch),
+    };
+    if (getParameter(trick, 'difficulty') !== currentGroup) {
+      currentGroup = getParameter(trick, 'difficulty');
+      result.push({ title: currentGroup, items: [] });
     }
-    if (getParameter(trick, "difficulty") === currentGroup) {
-      result[result.length - 1].items.push(trick)
-    }
-  }  
+    result[result.length - 1].items.push(searchItem);
+  }
+  return result;
 }
-*/
 
+const searchResult = ref<SearchResult>();
+
+watchEffect(async () => {
+  searchResult.value = await search();
+});
+
+function linkToDetails(primaryKey: PrimaryKey): string {
+  return `/tricks/${primaryKey[1]}/${primaryKey[0]}`;
+}
+
+/*
 const searchResult = ref<SearchResult>([
   {
     title: 'Level 1',
@@ -118,20 +131,26 @@ const searchResult = ref<SearchResult>([
     ],
   },
 ]);
+*/
 </script>
 
 <template>
   <DefaultLayout>
     <div class="w-full h-24 border border-red-500 text-center text-red-500">Search Parameters</div>
-    <div v-for="section in searchResult" class="w-full flex flex-col gap-2" :key="section.title">
-      <div class="text-lg px-3">{{ section.title }}</div>
-      <div class="grid grid-cols-3 gap-3 p-3 w-full">
-        <div
-          v-for="item in section.items"
-          :key="item.name"
-          class="p-1 border border-border text-center"
-        >
-          {{ item.name }}
+    <div class="w-full flex flex-col gap-5">
+      <div v-for="section in searchResult" class="w-full flex flex-col gap-1" :key="section.title">
+        <div class="text-lg font-medium px-3 w-full text-center">{{ section.title }}</div>
+        <div class="grid grid-cols-3 gap-2 p-2 w-full">
+          <OverviewCard
+            v-for="item in section.items"
+            :key="item.primaryKey[1] + ':' + item.primaryKey[0]"
+            :title="item.name"
+            :status="item.primaryKey[1]"
+            :stick-frequency="item.stickFrequency"
+            :is-favorite="item.isFavorite"
+            :is-new="item.isNew"
+            :link-to-details="linkToDetails(item.primaryKey)"
+          />
         </div>
       </div>
     </div>
