2b4e8becc654dff58582441f08a8642705663634
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2025-01-20 10:15:55 +0100

    Add more list columns on xl screens

--- src/routes/tricks/TrickList.vue
+++ src/routes/tricks/TrickList.vue
@@ -86,7 +86,7 @@ function linkToDetails(primaryKey: PrimaryKey): string {
     <div class="w-full flex flex-col gap-5">
       <div v-for="section in searchResult" class="w-full flex flex-col gap-1" :key="section.title">
         <div class="text-lg font-medium px-3 w-full text-center">{{ section.title }}</div>
-        <div class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 p-2 w-full">
+        <div class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 p-2 w-full">
           <OverviewCard
             v-for="item in section.items"
             :key="item.primaryKey[1] + ':' + item.primaryKey[0]"
