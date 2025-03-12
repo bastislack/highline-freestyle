ae37a4e4ed8b9ceb19797ab3c9fb4981d7482e04
Juan Andrade <juangabriel4699@gmail.com>
2025-03-12 15:32:22 -0300

    Fix inconsistencies

--- src/components/ui/form/FormMessage.vue
+++ src/components/ui/form/FormMessage.vue
@@ -1,16 +1,15 @@
 <script lang="ts" setup>
 import { ErrorMessage } from 'vee-validate';
-import { toValue, computed } from 'vue';
+import { toValue } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 import messagesErrors from '@/i18n/error';
-import { i18nMerge } from '@/i18n/i18nmerge';
 
 import { useFormField } from './useFormField';
 
 const { name, formMessageId } = useFormField();
 const { t } = useI18n({
-  messages: i18nMerge(messagesErrors),
+  messages: messagesErrors,
   scope: 'local',
 });
 
@@ -18,14 +17,6 @@ const props = defineProps<{
   // This is used to pass values for the message translation interpolation
   values?: Record<string, string>;
 }>();
-
-// Create a computed function to translate messages
-const translateMessage = computed(() => {
-  return (message: string) => {
-    if (!message) return '';
-    return t(message, props.values || {});
-  };
-});
 </script>
 
 <template>
@@ -36,6 +27,6 @@ const translateMessage = computed(() => {
     class="text-sm font-medium text-destructive"
     v-slot="{ message }"
   >
-    {{ message ? translateMessage(message) : '' }}
+    {{ message ? t(message, props.values || {}) : '' }}
   </ErrorMessage>
 </template>
