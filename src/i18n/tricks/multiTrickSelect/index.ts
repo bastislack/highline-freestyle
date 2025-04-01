389695b76052e2b922d00a5ec4113ce8e72c1cd9
Juan Andrade <juangabriel4699@gmail.com>
2025-04-01 09:55:42 -0300

    #380 Portuguese translation for MultiTrickSelectInternal.vue

--- src/i18n/tricks/multiTrickSelect/index.ts
+++ src/i18n/tricks/multiTrickSelect/index.ts
@@ -1,5 +1,6 @@
 import en from './multiTrickSelect.en.json';
 import es from './multiTrickSelect.es.json';
 import fr from './multiTrickSelect.fr.json';
+import pt from './multiTrickSelect.pt.json';
 
-export default { en, es, fr };
+export default { en, es, fr, pt };
