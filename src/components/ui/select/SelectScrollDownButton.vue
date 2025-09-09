a657625d4995bec7ae7af86569a3e8eb52b0ed4e
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2025-09-09 22:35:18 +0200

    refactor: import statement to make prettier happy

--- src/components/ui/select/SelectScrollDownButton.vue
+++ src/components/ui/select/SelectScrollDownButton.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 import { type HTMLAttributes, computed } from 'vue';
-import {
-  SelectScrollDownButton,
-  type SelectScrollDownButtonProps,
-  useForwardProps,
-} from 'reka-ui';
+import { SelectScrollDownButton, type SelectScrollDownButtonProps, useForwardProps } from 'reka-ui';
 import { ChevronDown } from 'lucide-vue-next';
 import { cn } from '@/lib/utils';
 
