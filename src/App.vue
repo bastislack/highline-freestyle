49d9ac7b51a1fce7cf1b1ff20c4f342660c83f9b
Julian Dietzel <33333283+JulianDietzel@users.noreply.github.com>
2025-09-14 12:36:28 +0200

    fix: reload view after loading tricks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import Toaster from './components/ui/toast/Toaster.vue';
+
+// Only used for rerendering the RouterView component
+const routerViewKey = ref(0);
+
+function forceRerender() {
+  routerViewKey.value += 1;
+}
 
 onMounted(async () => {
   // This imports the migration procedure that syncs the offical tricks and combos
   // Note that this is done in an async fashion. This way, that group of modules
   // can be split off the main bundle and be imported asynchronously.
-  (await import('./lib/database/official/index')).default();
+  await (await import('./lib/database/official/index')).default();
+  forceRerender();
 });
 </script>
 
 <template>
-  <RouterView />
+  <RouterView :key="routerViewKey" />
   <Toaster />
 </template>
