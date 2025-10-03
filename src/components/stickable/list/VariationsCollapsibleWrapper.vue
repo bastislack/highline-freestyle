942d212475396d964fe3eac080638db1fca8dbd1
Sebastian Egger <sebastian.egger@zollsoft.de>
2025-10-03 13:00:53 +0200

    fix: Show parent cards the same as other cards

--- src/components/stickable/list/VariationsCollapsibleWrapper.vue
+++ src/components/stickable/list/VariationsCollapsibleWrapper.vue
@@ -7,29 +7,26 @@ const isOpen = ref(false);
 </script>
 
 <template>
-  <Collapsible v-model:open="isOpen" class="col-span-full">
-    <div class="grid grid-cols-subgrid">
-      <div class="relative">
-        <!-- // Parent -->
-        <slot></slot>
+  <Collapsible v-model:open="isOpen" class="contents">
+    <div class="relative">
+      <!-- Parent -->
+      <slot></slot>
 
-        <CollapsibleTrigger as-child class="absolute top-1 left-1 z-20">
-          <button
-            @click.prevent
-            class="h-6 w-6 rounded-sm flex items-center justify-center transition-transform duration-200"
-            :class="{ 'rotate-180': isOpen }"
-          >
-            <Icon icon="ic:round-keyboard-arrow-down" class="h-5 w-5" />
-          </button>
-        </CollapsibleTrigger>
-      </div>
-
-      <CollapsibleContent class="col-span-full">
-        <div class="mt-2 mb-1 grid grid-cols-subgrid col-span-full gap-2">
-          <!-- // List of variation cards -->
-          <slot name="variations"></slot>
-        </div>
-      </CollapsibleContent>
+      <CollapsibleTrigger as-child class="absolute top-1 left-1 z-20">
+        <button
+          @click.prevent
+          class="h-6 w-6 rounded-sm flex items-center justify-center transition-transform duration-200"
+          :class="{ 'rotate-180': isOpen }"
+        >
+          <Icon icon="ic:round-keyboard-arrow-down" class="h-5 w-5" />
+        </button>
+      </CollapsibleTrigger>
     </div>
+
+    <CollapsibleContent class="col-span-full">
+      <div class="mt-2 mb-1 grid grid-cols-subgrid gap-2">
+        <slot name="variations"></slot>
+      </div>
+    </CollapsibleContent>
   </Collapsible>
 </template>
