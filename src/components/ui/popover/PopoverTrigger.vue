c1a4eae298a0cf4c6fc441585f49ee0f4784b954
Sebastian Egger <sebastian.egger@zollsoft.de>
2025-11-02 13:29:32 +0100

    fix: Fix formatting

--- src/components/ui/popover/PopoverTrigger.vue
+++ src/components/ui/popover/PopoverTrigger.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import type { PopoverTriggerProps } from "reka-ui"
-import { PopoverTrigger } from "reka-ui"
+import type { PopoverTriggerProps } from 'reka-ui';
+import { PopoverTrigger } from 'reka-ui';
 
-const props = defineProps<PopoverTriggerProps>()
+const props = defineProps<PopoverTriggerProps>();
 </script>
 
 <template>
