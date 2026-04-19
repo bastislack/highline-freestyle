8b986da30c1945ade0d35e3c47f3f5c2c414ea90
Sebastian Egger <sebastian.egger@zollsoft.de>
2026-04-19 18:58:07 +0200

    fix: TrickDetails are scrolled to the top when entering

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 export default createRouter({
   history: createWebHistory(),
+  scrollBehavior() {
+    return { top: 0 };
+  },
   routes: [
     {
       path: '/',
